4cc97673ede0e3a41ec102a0640d6d38f11994d4
Aisselvinod <vinodh@aissel.com>
2016-05-24 17:54:33 +0530

    first commit

---
+++ application/config/forum.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Forum config
+|--------------------------------------------------------------------------
+|
+| This determines the basic forum config
+|
+*/
+$config['site_title'] = 'Aissel Forum';
+$config['site_slogan'] = 'aissel';
+$config['site_email'] = 'vinodh@aissel.com';
